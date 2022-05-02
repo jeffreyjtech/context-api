a9ca739b8f9e1c72f71b4c0a4a3be2c67ab878b3
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-02 12:34:50 -0700

    Added form input to change dark/light display mode

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,39 @@
 import { useContext, useState, useEffect } from 'react';
 import { ThemeContext } from '../../context/theme';
-import { Card, Icon, Switch } from '@blueprintjs/core';
+import { Button, Card, Icon, Switch } from '@blueprintjs/core';
 
 function Header() {
   // This is React hooks equivalent of wrapping a component in a MyContext.Consumer component
   const theme = useContext(ThemeContext);
   const [checked, setChecked] = useState(false)
   const [style, setStyle] = useState(theme.mode === 'light' ? null : 'bp4-dark');
+  const [textInput, setTextInput] = useState('')
 
   function handleCheck(e) {
     setChecked(!checked);
+    theme.toggleMode(checked ? 'light' : 'dark');
+  }
+
+  function handleInput(e) {
+    e.preventDefault();
+    setTextInput(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Submitting:', textInput);
+    theme.toggleMode(textInput);
   }
 
   useEffect(() => {
-    theme.setMode(checked ? 'light' : 'dark')
     setStyle(theme.mode === 'light' ? null : 'bp4-dark');
-  }, [theme, checked]);
+    setTextInput('');
+  }, [theme]);
+
+  // useEffect(() => {
+    // theme.toggleMode(checked ? 'light' : 'dark');
+  // }, [checked]);
+  
 
   return (
     <div id="header">
@@ -23,6 +41,10 @@ function Header() {
         <h2>Settings</h2>
         <Icon icon="contrast" />
         <Switch label={`${theme.mode} Mode`} checked={checked} onChange={handleCheck} />
+        <form onSubmit={handleSubmit}>
+          <input placeholder='Enter display mode' value={textInput} onChange={handleInput} />
+          <Button type='submit'>Change mode</Button>
+        </form>
       </Card>
     </div>
   )
