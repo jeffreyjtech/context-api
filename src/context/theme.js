708d8fe5e7c79524976defc0f0b6b5c91e5cad8e
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-02 12:34:30 -0700

    Added a function to validate mode state changes

--- src/context/theme.js
+++ src/context/theme.js
@@ -8,10 +8,21 @@ function Theme({ children }) {
   // If we hardcoded this variable, it would work but it wouldn't do very much.
   const [mode, setMode] = useState('light');
 
+  const toggleMode = (newMode) => {
+    switch (newMode) {
+      case 'light': setMode(newMode); break;
+      case 'dark': setMode(newMode); break;
+      default:/*do nothing*/;
+    }
+  }
+
+
   return (
     // The value prop is THE prop for conveying data to context consumers
-    // Putting a state's setter function into this object is super danger
-    <ThemeContext.Provider value={{ mode, setMode }}>
+    // Putting a state's setter function into this object eg: { mode, setMode } is super danger 
+    // The toggleMode function validates the input before invoking setMode with the input
+    // Wrapper/validator functions like this are probably a good play?
+    <ThemeContext.Provider value={{ mode, toggleMode }}>
       {children}
     </ThemeContext.Provider>
   )
